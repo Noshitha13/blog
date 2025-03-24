b90ce3eec2e0f2ff9b9da7f4ca7622d705ad494e
Noshitha13 <104014321+Noshitha13@users.noreply.github.com>
2025-03-24 15:10:03 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,39 +1,21 @@
-require("dotenv").config(); 
+require("dotenv").config();
+const isDocker = process.env.DB_HOST === "db"; // Check if running inside Docker
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,  
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: "database_test",
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "blogdb",
+    host: isDocker ? "db" : "127.0.0.1", // Use "db" inside Docker, "127.0.0.1" locally
+    dialect: process.env.DB_DIALECT || "mysql",
+    logging: false, // Disable logging
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "database_production",
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  }
+    dialect: process.env.DB_DIALECT || "mysql",
+    logging: false, // Disable logging
+  },
 };
-
-//require("dotenv").config();
-
-//const isDocker = process.env.DB_HOST === "db";  // Check if running inside Docker
-
-//module.exports = {
-  //development: {
-    //username: process.env.DB_USERNAME || "root",
-    //password: process.env.DB_PASSWORD || "root",
-    //database: process.env.DB_NAME || "blogdb",
-    //host: isDocker ? "db" : "127.0.0.1",  // Use "db" inside Docker, "127.0.0.1" locally
-    //dialect: process.env.DB_DIALECT || "mysql"
-  //}
-//};
